6ae086f96e690a1786204a208fab00b394332e05
Ghost <faydaen@inbox.ru>
2019-05-12 01:59:27 +0300

    хтмл

--- index.php
+++ index.php
@@ -2,18 +2,18 @@
 
 
 $data = [
-    [
-        'title' => 'В каком случае водитель совершит вынужденную остановку?',
-        'img' => '',
-        'answers' => [
-            '1. Остановившись непосредственно перед пешеходным переходом, чтобы уступить дорогу пешеходу.',
-            '2. Остановившись на проезжей части из-за технической неисправности транспортного средства.',
-            '3. В обоих перечисленных случаях.'
-        ],
-        'right_answer' => 1,
-        'comment' => '',
-        'tags' => [],
-    ],
+//    [
+//        'title' => 'В каком случае водитель совершит вынужденную остановку?',
+//        'img' => '',
+//        'answers' => [
+//            '1. Остановившись непосредственно перед пешеходным переходом, чтобы уступить дорогу пешеходу.',
+//            '2. Остановившись на проезжей части из-за технической неисправности транспортного средства.',
+//            '3. В обоих перечисленных случаях.'
+//        ],
+//        'right_answer' => 1,
+//        'comment' => '',
+//        'tags' => [],
+//    ],
     [
         'title' => 'Разрешен ли Вам съезд на дорогу с грунтовым покрытием?',
         'img' => 'n1_2.jpg',
@@ -41,6 +41,11 @@ $data = [
 //        'tags' => [],
 //    ]
 //}
+
+
+
+$b = $data[array_rand($data)];
+
 ?>
 
 <!doctype html>
@@ -48,7 +53,7 @@ $data = [
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Пдд</title>
+    <title>ПДД</title>
     <link rel="stylesheet" href="./css/bulma.min.css">
 </head>
 <body>
@@ -56,6 +61,33 @@ $data = [
 
 
 
+<div class="container">
+    <div class="notification">
+        <form method="get">
+        <?php if(!empty($b['img'])): ?>
+            <img src="./img/<?= $b['img'] ?>">
+            <br>
+        <?php endif ?>
+        <div>
+            <strong><?= $b['title'] ?></strong>
+        </div>
+        <br>
+
+        <?php foreach ($b['answers'] as $answer): ?>
+            <button type="submit" style="text-align: left; display: block; width: 100%" class="button"><?= $answer ?></button><br>
+        <?php endforeach; ?>
+        </form>
+
+
+        
+    </div>
+</div>
+
+<script>
+    function showTips() {
+
+    }
+</script>
 
 </body>
 </html>
